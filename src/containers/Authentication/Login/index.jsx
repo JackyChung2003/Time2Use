7bdf891a738c2f9aeeca61fa337a10d1305b09d2
YIPINNN <117891859+YIPINNN@users.noreply.github.com>
2025-01-17 01:45:40 +0800

    Update index.jsx
    Enhance the login page animation

--- src/containers/Authentication/Login/index.jsx
+++ src/containers/Authentication/Login/index.jsx
@@ -100,6 +100,7 @@ const Login = () => {
             <div className="floating-shape shape-1"></div>
             <div className="floating-shape shape-2"></div>
             <div className="floating-shape shape-3"></div>
+            <div className="floating-shape shape-4"></div>
 
             <h1 className="header">Login</h1>
             <form onSubmit={handleEmailLogin}>
